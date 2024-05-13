261e086317e318ce725b74655aaf3c5a742cd861
Sandeep <sandeepkbh47@gmail.com>
2024-05-14 00:20:49 +0530

    nchange

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('todo-input')
+const todoList = document.getElementById('todo-list')
+
+function addTodo(event) {
+  console.log("hgdh")
+  event.preventDefault()
+  const newTodo = input.value.trim()
+
+  if (newTodo) {
+    const li = document.createElement('li')
+    li.textContent = newTodo
+    todoList.addEventListener('click', (e) => {
+      this.removeChild(e.target)
+    })
+
+    todoList.appendChild(li)
+    input.value = ''
+  } else {
+    console.log('Please enter a task!')
+  }
+}
